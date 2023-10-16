3b345f9a90428abb0e14bb7936564c7c487c5b1f
Anton Pos <antohapos@gmail.com>
2023-10-16 22:29:32 +0300

    task4

---
+++ lesson4 abstraction encapsulation/task1/task1 adress/task1 adress/task1 adress.cpp
@@ -0,0 +1,61 @@
+﻿
+
+#include <iostream>
+#include <fstream>
+
+class Adress {
+private:
+    std::string city;
+    std::string street;
+    int building;
+    int apartment;
+public:
+    Adress(std::string city, std::string street, int building, int apartment)
+    {
+        this->city = city;
+        this->street = street;
+        this->building = building;
+        this->apartment = apartment;
+    }
+    std::string get_city() { return city; }
+    std::string get_street() { return street; }
+    int get_building() { return building; }
+    int get_apartment() { return apartment; }
+};
+
+
+
+int main()
+{
+    system("chcp 1251");
+    std::ifstream in("in.txt");
+    std::ofstream out("out.txt");
+
+    if (in.is_open()) {
+        int size = 0;
+        std::string city;
+        std::string street;
+        int building;
+        int apartment;
+        in >> size;
+        out << size << std::endl;
+        Adress** arr = new Adress * [size];
+
+        for (int n = 0; n < size; n++) {
+            in >> city;
+            in >> street;
+            in >> building;
+            in >> apartment;
+            arr[n] = new Adress(city, street, building, apartment);
+        };
+
+        for (int n = size - 1; n >= 0; n--) {
+            out << arr[n]->get_city() << ", " << arr[n]->get_street() << ", " << arr[n]->get_building() << ", " << arr[n]->get_apartment() << std::endl;
+        }
+        delete[] arr;
+        in.close();
+        out.close();
+    }
+    else { std::cout << "файл in не найден"; }
+}
+
---
+++ lesson4 abstraction encapsulation/task2/task2 sorting/task2 sorting/task2 sorting.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+
+class Address {
+private:
+    std::string city;
+    std::string street;
+    int building;
+    int apartment;
+public:
+    Address(std::string city, std::string street, int building, int apartment){
+        this->city = city;
+        this->street = street;
+        this->building = building;
+        this->apartment = apartment;
+    }
+    std::string get_full_address(){
+        return city + " " + street + " " + std::to_string(building) + " " + std::to_string(apartment);
+
+    } 
+    std::string get_city() { return city; }
+    
+};
+
+
+int main(){
+    system("chcp 1251");
+    std::ifstream in("in.txt");
+    std::ofstream out("out.txt");
+    if (in.is_open()) {
+        int size = 0;
+        std::string city;
+        std::string street;
+        int building;
+        int apartment;
+        in >> size;
+        out << size << std::endl;
+        Address** arr = new Address * [size];
+        for (int n = 0; n < size; n++) {
+            in >> city;
+            in >> street;
+            in >> building;
+            in >> apartment;
+            arr[n] = new Address(city, street, building, apartment);
+        };
+        for (int j = 0; j < size - 1 ; j++){
+            for (int i = 0; i < size - 1; i++) {
+                if (arr[i]->get_city()[0] < arr[i + 1]->get_city()[0]) {std::swap(arr[i], arr[i + 1]);}
+            }
+        }
+        for (int n = size - 1; n >= 0; n--) {out << arr[n]->get_full_address() << std::endl;}
+        delete[] arr;
+        in.close();
+        out.close();
+    }
+    else { std::cout << "файл in не найден"; } 
+}
