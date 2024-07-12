3349f32276d4263e1c8a3ce111db2da845bf9015
Anton Pos <antohapos@gmail.com>
2024-07-12 12:10:56 +0300

    TamplatesAndFunctorsСorrections

--- lesson 14 tamplates/Table/Table/Table.cpp
+++ lesson 14 tamplates/Table/Table/Table.cpp
@@ -13,6 +13,36 @@ public:
         }
     }
 
+    // копирование
+    table(const table& other) : rows(other.rows), cols(other.cols) {
+        data = new T * [rows];
+        for (int i = 0; i < rows; ++i) {
+            data[i] = new T[cols];
+            for (int j = 0; j < cols; ++j) {
+                data[i][j] = other.data[i][j];
+            }
+        }
+    }
+    // присвоение
+    table& operator=(const table& other) {
+        if (this != &other) {
+            for (int i = 0; i < rows; ++i) {
+                delete[] data[i];
+            }
+            delete[] data;
+            rows = other.rows;
+            cols = other.cols;
+            data = new T * [rows];
+            for (int i = 0; i < rows; ++i) {
+                data[i] = new T[cols];
+                for (int j = 0; j < cols; ++j) {
+                    data[i][j] = other.data[i][j];
+                }
+            }
+        }
+        return *this;
+    }
+
     ~table() {
         for (int i = 0; i < rows; ++i) {
             delete[] data[i];
@@ -31,14 +61,22 @@ public:
     std::pair<int, int> Size() const {
         return std::make_pair(rows, cols);
     }
-    
 };
 
 int main() {
+    system("chcp 1251");
     auto test = table<int>(2, 3);
     test[0][0] = 4;
-    std::cout << test[0][0];
+    std::cout << test[0][0] << std::endl;
     std::pair<int, int> size = test.Size();
-    std::cout << "Table size: " << size.first << "x" << size.second << std::endl;
+    std::cout << "Размер таблицы: " << size.first << "x" << size.second << std::endl;
+    auto test2 = test;
+    //test2[0][0] = 5;
+    std::cout << test2[0][0] << std::endl;
+    table<double> t1(2, 3);
+    table<double> t3(t1);
+    size = t3.Size();
+    std::cout << "Размер таблицы: " << size.first << "x" << size.second << std::endl;
     return 0;
+
 }
--- lesson 14 tamplates/squared/squared/squared.cpp
+++ lesson 14 tamplates/squared/squared/squared.cpp
@@ -8,26 +8,33 @@ T square(T x) {
 }
 
 template <typename T>
-T square(const std::vector<T>& v, int i) {
-    return v[i] * v[i];
+std::vector<T> square(const std::vector<T>& v) {
+    std::vector<T> squared_v;
+    for (const auto& element : v) {
+        squared_v.push_back(element * element);
+    }
+    return squared_v;
 }
+
 
 int main() {
     int a = 4;
-    std::vector<int> v = { -1, 4, 8, };
-
     std::cout << "[in]: " << a << std::endl;
-    std::cout << "[out]: " << square(a) << std::endl;
 
+    int a2 = square(a);
+    std::cout << "[out]: " << a2 << std::endl;
+
+    std::vector<int> v = { -1, 4, 8, };
     std::cout << "[in]: ";
     for (int i = 0; i < v.size(); i++) {
         if (i < v.size() - 1)std::cout << v[i] << ", ";
         else std::cout << v[i] << std::endl;
     }
 
+    std::vector<int> v2 = square(v);
     std::cout << "[out]: ";
-    for (int i = 0; i < v.size(); i++) {
-        if (i < v.size() - 1) { std::cout << square(v, i) << ", "; }
-        else std::cout << square(v, i);
+    for (int i = 0; i < v2.size(); i++) {
+        if (i < v2.size() - 1)std::cout << v2[i] << ", ";
+        else std::cout << v2[i] << std::endl;
     }
 }
